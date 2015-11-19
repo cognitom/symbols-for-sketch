4fe90f2e0e095a4e966ba03876b7ecf0ab05b7f4
weitsai <begining1003@gmail.com>
2015-11-19 21:20:38 +0800

    Add gulp symbols-android
    Add android string xml

--- gulpfile.js
+++ gulpfile.js
@@ -24,10 +24,6 @@ gulp.task('symbols', function(){
         fontPath: '../fonts/', // set path to font (from your CSS file if relative)
         className: 's' // set class name in your CSS
       };
-      gulp.src('templates/' + template + '.xml')
-        .pipe(consolidate('lodash', options))
-        .pipe(rename({ basename:fontName }))
-        .pipe(gulp.dest('dist/xml/')); // set path to export your xml
 
       gulp.src('templates/' + template + '.css')
         .pipe(consolidate('lodash', options))
@@ -39,6 +35,29 @@ gulp.task('symbols', function(){
         .pipe(consolidate('lodash', options))
         .pipe(rename({ basename:'sample' }))
         .pipe(gulp.dest('dist/')); // set path to export your sample HTML
+    })
+    .pipe(gulp.dest('dist/fonts/')); // set path to export your fonts
+});
+
+gulp.task('symbols-android', function(){
+  gulp.src('symbol-android-16px.sketch') // you can also choose 'symbol-font-16px.sketch'
+    .pipe(sketch({
+      export: 'artboards',
+      formats: 'svg'
+    }))
+    .pipe(iconfont({ fontName: fontName }))
+    .on('glyphs', function(glyphs) {
+      var options = {
+        glyphs: glyphs.map(function(glyph) {
+          // this line is needed because gulp-iconfont has changed the api from 2.0
+          return { name: glyph.name, codepoint: glyph.unicode[0].charCodeAt(0) }
+        }),
+        fontName: fontName
+      };
+      gulp.src('templates/' + template + '.xml')
+        .pipe(consolidate('lodash', options))
+        .pipe(rename({ basename:fontName }))
+        .pipe(gulp.dest('dist/xml/')); // set path to export your xml
     })
     .pipe(gulp.dest('dist/fonts/')); // set path to export your fonts
 });
