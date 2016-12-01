f61e53f1adb18efc51a9bcbba1a68fa2603d6017
Tsutomu Kawamura <cognitom@gmail.com>
2016-12-01 13:36:30 +0900

    Standardizes coding style

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,16 @@
-const
-  gulp = require('gulp'),
-  rename = require('gulp-rename'),
-  sketch = require('gulp-sketch'),
-  iconfont = require('gulp-iconfont'),
-  consolidate = require('gulp-consolidate')
+const gulp = require('gulp')
+const rename = require('gulp-rename')
+const sketch = require('gulp-sketch')
+const iconfont = require('gulp-iconfont')
+const consolidate = require('gulp-consolidate')
 
 /**
  * Font settings
  */
-const
-  // set name of your symbol font
-  fontName = 'symbols',
-  // set class name in your CSS
-  className = 's',
-  // you can also choose 'foundation-style'
-  template = 'fontawesome-style',
-  // you can also choose 'symbol-font-16px.sketch'
-  skethcFileName = 'symbol-font-14px.sketch'
+const fontName = 'symbols' // set name of your symbol font
+const className = 's' // set class name in your CSS
+const template = 'fontawesome-style' // or 'foundation-style'
+const skethcFileName = 'symbol-font-14px.sketch' // or 'symbol-font-16px.sketch'
 
 /**
  * Recommended to get consistent builds when watching files
@@ -43,13 +37,13 @@ gulp.task('symbols', () =>
         fontPath: '../fonts/', // set path to font (from your CSS file if relative)
         glyphs: glyphs.map(mapGlyphs)
       }
-      gulp.src(`templates/${ template }.css`)
+      gulp.src(`templates/${template}.css`)
         .pipe(consolidate('lodash', options))
         .pipe(rename({ basename: fontName }))
         .pipe(gulp.dest('dist/css/')) // set path to export your CSS
 
       // if you don't need sample.html, remove next 4 lines
-      gulp.src(`templates/${ template }.html`)
+      gulp.src(`templates/${template}.html`)
         .pipe(consolidate('lodash', options))
         .pipe(rename({ basename: 'sample' }))
         .pipe(gulp.dest('dist/')) // set path to export your sample HTML
@@ -62,6 +56,6 @@ gulp.task('watch', () => gulp.watch('*.sketch', ['symbols']))
 /**
  * This is needed for mapping glyphs and codepoints.
  */
-function mapGlyphs(glyph) {
+function mapGlyphs (glyph) {
   return { name: glyph.name, codepoint: glyph.unicode[0].charCodeAt(0) }
 }
